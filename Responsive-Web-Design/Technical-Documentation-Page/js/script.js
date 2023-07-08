a114cb72735439ff21a2f6553626a16247258c4a
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-07-07 23:27:49 -0300

    Technical-Documentation-Page update menu

--- Responsive-Web-Design/Technical-Documentation-Page/js/script.js
+++ Responsive-Web-Design/Technical-Documentation-Page/js/script.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById("menu-button");
 const navBar = document.getElementById("nav-bar");
+const mainDoc = document.getElementById("main-doc");
 
 let isMenuOpen = true;
 
@@ -8,16 +9,17 @@ menuButton.addEventListener("click", () => {
 
   if (isMenuOpen === false) {
     menuButton.innerHTML = "<i class='fa-solid fa-bars'></i>";
-    navBar.classList.add('hidden');
+    navBar.classList.add("hidden");
+    mainDoc.classList.remove("effect");
     setTimeout(() => {
       navBar.style.display = "none";
     }, 100);
   } else {
     menuButton.innerHTML = "<i class='fa-solid fa-xmark'></i></i>";
     navBar.style.display = "flex";
-    window.scrollTo(0, 0)
     setTimeout(() => {
-      navBar.classList.remove('hidden')
+      navBar.classList.remove("hidden")
+      mainDoc.classList.add("effect");
     }, 100);
   }
 });
