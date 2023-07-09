ef446891188dc4f786197d9446f0bc7bbce351c4
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-07-08 21:05:10 -0300

    Technical-Documentation-Page update menu

--- Responsive-Web-Design/Technical-Documentation-Page/js/script.js
+++ Responsive-Web-Design/Technical-Documentation-Page/js/script.js
@@ -1,25 +1,48 @@
 const menuButton = document.getElementById("menu-button");
 const navBar = document.getElementById("nav-bar");
-const mainDoc = document.getElementById("main-doc");
+const navLinks = document.querySelectorAll(".nav-link");
 
-let isMenuOpen = true;
+let isMenuOpen;
+
+const closeMenu = () => {
+  isMenuOpen = false
+  menuButton.innerHTML = "<i class='fa-solid fa-bars'></i>";
+  navBar.classList.add("hidden");
+  setTimeout(() => {
+    navBar.style.display = "none";
+  }, 100);
+};
+
+const openMenu = () => {
+  isMenuOpen = true
+  menuButton.innerHTML = "<i class='fa-solid fa-xmark'></i></i>";
+    navBar.style.display = "flex";
+    setTimeout(() => {
+      navBar.classList.remove("hidden")
+    }, 100);
+};
+
+const mobileMediaQuery = window.matchMedia("(max-width: 730px)")
+if (mobileMediaQuery.matches) {
+  closeMenu();
+} else {
+  openMenu();
+};
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (isMenuOpen && mobileMediaQuery.matches) {
+      closeMenu();
+    };
+  });
+});
 
 menuButton.addEventListener("click", () => {
   isMenuOpen = !isMenuOpen;
 
   if (isMenuOpen === false) {
-    menuButton.innerHTML = "<i class='fa-solid fa-bars'></i>";
-    navBar.classList.add("hidden");
-    mainDoc.classList.remove("effect");
-    setTimeout(() => {
-      navBar.style.display = "none";
-    }, 100);
+    closeMenu();
   } else {
-    menuButton.innerHTML = "<i class='fa-solid fa-xmark'></i></i>";
-    navBar.style.display = "flex";
-    setTimeout(() => {
-      navBar.classList.remove("hidden")
-      mainDoc.classList.add("effect");
-    }, 100);
-  }
+    openMenu();
+  };
 });
