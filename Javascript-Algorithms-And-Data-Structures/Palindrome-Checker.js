6bf19a193b1713519f9ff2af8052ca6633ccd7bb
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-08-02 22:16:02 -0300

    Palindrome-Checker better algorithm

--- Javascript-Algorithms-And-Data-Structures/Palindrome-Checker.js
+++ Javascript-Algorithms-And-Data-Structures/Palindrome-Checker.js
@@ -1,18 +1,8 @@
 const palindrome = (str) => {
 
-  str = str.toLowerCase().replace(/[$_!,:#|-\s.()]/g, "");
+  str = str.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
 
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) !== str.charAt((str.length - 1 ) - i)) {
-      return false;
-    }
-
-    if (i >= Math.floor(str.length / 2)) {
-      return true;
-    }
-  }
-
-  return true;
+  return str.split('').reverse().join('') === str ? true : false;
 };
 
 console.log(palindrome('Sit on a potato pan, Otis!')); // true
